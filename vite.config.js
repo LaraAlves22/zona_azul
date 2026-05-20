4e8d1f5cc4249da63c9d3af24187415276931372
kellyr5 <kelly.l.reis@gmail.com>
2026-05-20 19:12:37 -0300

    Dashboard completo - motorista, fiscal e admin

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: { host: '0.0.0.0', proxy: { '/api': 'http://3.139.101.137:3000' } }
+  server: { host: '0.0.0.0', proxy: { '/api': 'http://3.150.159.219:3000' } }
 })
